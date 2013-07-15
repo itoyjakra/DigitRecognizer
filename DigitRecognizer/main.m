7d81e2843354b54ee86db268a43e0333d00497cc
arko <arkajyoti@gmail.com>
2013-07-14 21:44:49 -0500

    first running prototype with training set prediction

--- DigitRecognizer/main.m
+++ DigitRecognizer/main.m
@@ -3,18 +3,57 @@ clear ; close all; clc
 
 % read the training data
 % first row is column header, first column is label for the training set (actual digit)
-digitData = dlmread('train_short100.csv', ',', 1, 0); 
+trainData = dlmread('train_short10000.csv', ',', 1, 0); 
 
-input_layer_size = sqrt(size(digitData)(2)-1);  % input image is 28x28 pixels
+%input_layer_size = sqrt(size(trainData)(2)-1);  % input image is 28x28 pixels
+input_layer_size = size(trainData)(2)-1;  % input image is 28x28 pixels
 hidden_layer_size = 25;   % 25 hidden units
 num_labels = 10;          % 10 labels, from 1 to 10  ("0" is mapped to label 10)
+
+X = trainData(:,2:end);
+y = trainData(:,1);
+
+% map 0 to 10 in the labels
+%locs = find (y == 0)
+y( find(y==0) ) = 10;
+
 
 % initialize neural network parameters
 initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
 initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+size(initial_Theta1)
+size(initial_Theta2)
 
 % Unroll parameters
 initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
 
 %  Check gradients by running checkNNGradients
-checkNNGradients;
+% should be removed for actual run
+%checkNNGradients(0); % lambda=0
+%checkNNGradients(3); % lambda=3
+
+% train the neural network
+% TODO loop over MaxIter and lambda to optimize training
+options = optimset('MaxIter', 100);
+lambda = 2;
+
+costFunction = @(p) nnCostFunction(p, input_layer_size, hidden_layer_size, num_labels, X, y, lambda);
+tic
+[nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+toc
+
+% Obtain Theta1 and Theta2 back from nn_params
+Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+                 hidden_layer_size, (input_layer_size + 1));
+
+Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));
+% prediction on training set
+
+pred = predict(Theta1, Theta2, X);
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
+
+% prediction on the test data
+%testData = dlmread('test_short4000.csv', ',', 1, 0); 
+%testX = testData(:,1:end);
+%testPrediction = predict(Theta1, Theta2, testX);
