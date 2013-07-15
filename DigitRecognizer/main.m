ffccc32e2427ed785401f6879bb27504f52a45d1
arko <arkajyoti@gmail.com>
2013-07-14 22:51:19 -0500

    working prediction on test set

--- DigitRecognizer/main.m
+++ DigitRecognizer/main.m
@@ -34,7 +34,8 @@ initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
 
 % train the neural network
 % TODO loop over MaxIter and lambda to optimize training
-options = optimset('MaxIter', 100);
+% TODO check the effect of number of hidden layers and hidden layer size
+options = optimset('MaxIter', 500);
 lambda = 2;
 
 costFunction = @(p) nnCostFunction(p, input_layer_size, hidden_layer_size, num_labels, X, y, lambda);
@@ -54,6 +55,14 @@ pred = predict(Theta1, Theta2, X);
 fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
 
 % prediction on the test data
-%testData = dlmread('test_short4000.csv', ',', 1, 0); 
-%testX = testData(:,1:end);
-%testPrediction = predict(Theta1, Theta2, testX);
+testData = dlmread('test.csv', ',', 1, 0); 
+testX = testData(:,1:end);
+tic
+testPrediction = predict(Theta1, Theta2, testX);
+testPrediction( find(testPrediction==10) ) = 0;
+toc
+fid = fopen('DigitPredictions.csv', 'w')
+for i=1:size(testPrediction)
+    fprintf(fid, '%d,%d\n', i, testPrediction(i));
+end
+fclose(fid)
