75879e4512f2e77265ca0e5d71628f0a0bf1276e
Arkajyoti MIsra <arko@localhost.localdomain>
2013-08-05 14:11:11 -0500

    bugfix in cross validation

---
+++ DigitRecognizer/pca.m
@@ -0,0 +1,10 @@
+function [U, S] = pca(X)
+%PCA Run principal component analysis on the dataset X
+%   [U, S, X] = pca(X) computes eigenvectors of the covariance matrix of X
+%   Returns the eigenvectors U, the eigenvalues (on diagonal) in S
+%
+
+[m, n] = size(X);
+[U, S, V] = svd(X'*X/m);
+
+end
